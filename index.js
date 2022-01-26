88470c0fa7869a8348e5c696dc6750ffa8782b04
Etienne261 <93247779+Etienne261@users.noreply.github.com>
2022-01-26 16:32:06 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-
-  if (window.scrollY < 200) {
-    nav.style.top = "0";
-  } else {
-    nav.style.top = "-600px";
-  }
-});
